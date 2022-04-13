607da8a13f656e70d909acf20697f443524e057a
kreison <alankainazovr@gmail.com>
2022-04-13 21:34:22 +0600

    test

---
+++ lesson2/js/script.js
@@ -0,0 +1,66 @@
+// // 'use strict'
+// // let array = [
+// //     {
+// //         username: 'Jack',
+// //         fullName: 'jack kyrgyz'
+// //     },
+// //     {
+// //         username: 'Joe',
+// //         fullName: 'joe kyrgyz'
+// //     },
+// //     {
+// //         username: 'John',
+// //         fullName: 'john kyrgyz'
+// //     },
+// // ]
+// // // for (let i = 0; i < array.length; i++){
+// // //     console.log(array[i])
+// // // }
+// // // for ( let user of array){
+// // //
+// // // }
+// // const obj = {
+// //     key1: 'b1',
+// //     key2: 'b1',
+// //     key3: 'b1',
+// //     key4: 'b1',
+// //     key5: 'b1',
+// //     key6: 'b1',
+// //     key7: 'b1',
+// //     key8: 'b1',
+// //     key9: 'b1',
+// //     key10: 'b1',
+// // };
+// // for (let key in obj){
+// //     console.log(obj[key]);
+// // }
+// // let i = 0;
+// // while (i < 5){
+// //     console.log('bruh');
+// //     i
+// // }
+const users = [
+    {
+        username: 'jack',
+        salary: '500'
+    },
+    {
+        username: 'joe',
+        salary: '1000'
+    },
+    {
+        username: 'kiraYoshikage',
+        salary: '5000'
+    }
+]
+//
+// let filteredUsers = users.filter( user => user.salary > 500);
+//
+// // for (let user of users){
+// //     if (user.salary > 500){
+// //         filteredUsers.push(user);
+// //     }
+// // }
+// console.log(filteredUsers)
+const d = [];
+for (let)
