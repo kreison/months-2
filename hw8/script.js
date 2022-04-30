198f86ac7a37d836f09c3392a6c15ff14edcb3b6
kreison <75908447+kreison@users.noreply.github.com>
2022-05-01 01:05:25 +0600

    Delete hw8 directory

--- hw8/script.js
+++
@@ -1,5 +0,0 @@
-let block = document.getElementById('block');
-let hole = document.getElementById('hole');
-hole.addEventListener("animationiteration", ()=>{
-    let randomPos = ((Math.random()*300)*150)
-})
